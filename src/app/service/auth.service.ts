f3bf8b50835beebfed594ba1ecfdd740d9646486
ViniBavaresco <91674067+ViniBavaresco@users.noreply.github.com>
2021-12-09 14:55:21 -0300

    Update auth.service.ts

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -8,11 +8,14 @@ import { Usuario } from '../model/Usuario';
 @Injectable({
   providedIn: 'root',
 })
+
 export class AuthService {
   constructor(private http: HttpClient) { }
+
   entrar(userLogin: UserLogin): Observable<UserLogin> {
     return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin);
   }
+
   cadastrar(usuario: Usuario): Observable<Usuario> {
     return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario);
   }
