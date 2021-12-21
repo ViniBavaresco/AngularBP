c05c77b3327ab26e5a53293f489f8b65d01bf3a5
ViniBavaresco <91674067+ViniBavaresco@users.noreply.github.com>
2021-12-20 23:20:38 -0300

    Implementação de PUT de usuário

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -44,4 +44,7 @@ export class CadastrarComponent implements OnInit {
       })
     }
   }
+
+
+  
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -13,15 +13,19 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin> {
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin);
+    return this.http.post<UserLogin>('https://blogpessoalvini.herokuapp.com/usuarios/logar', userLogin);
   }
 
   cadastrar(usuario: Usuario): Observable<Usuario> {
-    return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario);
+    return this.http.post<Usuario>('https://blogpessoalvini.herokuapp.com/usuarios/cadastrar', usuario);
+  }
+
+  atualizar(usuario: Usuario): Observable<Usuario> {
+    return this.http.put<Usuario>('https://blogpessoalvini.herokuapp.com/usuarios/atualizar', usuario);
   }
 
   getByIdUser(id: number): Observable<Usuario> {
-    return this.http.get<Usuario>(`http://localhost:8080/usuarios/${id}`)
+    return this.http.get<Usuario>(`https://blogpessoalvini.herokuapp.com/usuarios/${id}`)
   }
 
 
